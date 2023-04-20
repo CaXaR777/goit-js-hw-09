e1f55f9109a17853d7840f94117684e47e9e95ff
CaXaR777 <116805047+CaXaR777@users.noreply.github.com>
2023-04-20 22:00:05 +0000

    Deploying to gh-pages from @ CaXaR777/goit-js-hw-09@9b2088b739348dcc56ccfec57f23ad1fadb833d0 🚀

---
+++ 01-color-switcher.63808113.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");console.log(d);let o=null;t.addEventListener("click",(a=>{t.disabled=!0,e.disabled=!1,o=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(d=>{clearInterval(o),e.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.63808113.js.map
